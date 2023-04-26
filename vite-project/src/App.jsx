08f56b70745e0b113cfabd34b94eff67b0690872
John Hurst <john@dandyfarm>
2023-04-26 19:37:47 -0400

    a very simple counter

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import "./App.css";
+import KataSection from "./components/KataSection";
 import EggList from "./components/EggList";
+import SimpleCounter from "./components/SimpleCounter";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div className="max-w-xl mx-auto mt-12">
-        <EggList eggs={["Cadbury", "Lindt", "Reeses"]}></EggList>
+      <div className="flex justify-around p-12">
+        <KataSection headerText="EggList">
+          <EggList eggs={["Cadbury", "Lindt", "Reeses"]}></EggList>
+        </KataSection>
+        <KataSection headerText="A Counter! Whee!!!">
+          <SimpleCounter></SimpleCounter>
+        </KataSection>
       </div>
     </div>
   );
