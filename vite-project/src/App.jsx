f0a32d2a749fa6cd04d0c0b7f7638047463c824b
John Hurst <fuzzybunnies@gmail.com>
2023-04-28 16:57:25 -0400

    set up next kata

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import JokeList from "./components/JokeList";
 import SantasList from "./components/SantasList";
 import KataSection from "./components/KataSection";
 import EggList from "./components/EggList";
@@ -11,6 +12,9 @@ function App() {
   return (
     <div className="App">
       <div className="flex flex-wrap justify-around p-4 space-y-8">
+        <KataSection headerText="A List of Jokes">
+          <JokeList></JokeList>
+        </KataSection>
         <KataSection headerText="Santa's List">
           <SantasList send={(data) => console.log(data)}></SantasList>
         </KataSection>
---
+++ vite-project/src/components/JokeList.jsx
@@ -0,0 +1,19 @@
+// Render the following jokes from the json to a couple of simply formatted boxes on the screen
+// at least each joke box should be it own component
+// [
+// 	{
+// 		id: 1,
+// 		setup: "What's the best thing about a Boolean?",
+// 		punchline: "Even if you're wrong, you're only off by a bit",
+//    rating: 4
+// 	},
+// 	{
+// 		id: 2,
+// 		setup: "Why do programmers wear glasses?",
+// 		punchline: "Because they need to C#"
+//    rating: 2
+// 	}
+// ]
+export default function JokeList() {
+  return <div>Not implemented</div>;
+}
