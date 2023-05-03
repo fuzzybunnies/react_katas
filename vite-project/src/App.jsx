44b06034b4315cbc24566f8603e263d77c3749b0
John Hurst <fuzzybunnies@gmail.com>
2023-05-03 06:50:21 -0400

    updated katas

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import DollarsPerDay from "./components/spending_reports/DollarsPerDay";
 import JokeList from "./components/JokeList";
 import SantasList from "./components/SantasList";
 import KataSection from "./components/KataSection";
 import EggList from "./components/EggList";
 import SimpleCounter from "./components/SimpleCounter";
+import CsvToJson from "./components/csv_to_json/CsvToJson";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +14,12 @@ function App() {
   return (
     <div className="App">
       <div className="flex flex-wrap justify-around p-4 space-y-8">
+        <KataSection headerText="Daily Spend data">
+          <DollarsPerDay></DollarsPerDay>
+        </KataSection>
+        <KataSection headerText="Csv to Json">
+          <CsvToJson></CsvToJson>
+        </KataSection>
         <KataSection headerText="A List of Jokes">
           <JokeList></JokeList>
         </KataSection>
