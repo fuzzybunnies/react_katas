50aba536f2a2f90246da6a4f607c368da5402e1f
John Hurst <fuzzybunnies@gmail.com>
2023-05-03 06:48:49 -0400

    new kata for converting a csv to json

---
+++ vite-project/src/components/csv_to_json/CsvToJson.jsx
@@ -0,0 +1,9 @@
+/* 
+Create a component with a single input that receives some CSV text
+Have the component transform it to JSON
+Display the JSON in another component
+*/
+
+export default function CsvToJson() {
+  return <div>Not implemented</div>;
+}
